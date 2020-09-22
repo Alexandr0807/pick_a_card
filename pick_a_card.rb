1aa7631143018e0eb6d53d73212245da672f41dd
Alexandr0807 <Zelinski080796@gmail.com>
2020-09-22 10:03:07 +0300

    multiple_cards

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -2,4 +2,34 @@
 values = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
 suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades'] # Буби, Черви, Крести, Пики
 
-puts "#{values.sample} of #{suits.sample}"
+puts "#{values.sampleg gf w{suits.sample}"cards=[]
+vulues.each do|value|
+	suits.each do |suit|
+		cards <<”#{value} of #{suit}”
+	end
+end
+
+cards.shuffle!
+
+puts ’How many cards do you pick?’
+number =STDIN.gets.to_i
+
+number.times do
+	put cards.pop
+end
+cards=[]
+vulues.each do|value|
+	suits.each do |suit|
+		cards <<”#{value} of #{suit}”
+	end
+end
+
+cards.shuffle!
+
+puts ’How many cards do you pick?’
+number =STDIN.gets.to_i
+
+number.times do
+	put cards.pop
+end
+
